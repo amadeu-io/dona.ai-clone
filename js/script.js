6e00972d3411e3be4675796b25ef07f8634d4ea9
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-04-17 13:58:18 -0500

    Create initial idea
    So far the app defines a Task class that creates task objects that
    contain title & completed, and also have the methods toggle() &
    render().
    
    The toggle() method will change the status of completed, and the
    render() method will render the object to screen via DOM manipulation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,48 @@
-const dummy = document.querySelector(".dummy");
-dummy.addEventListener("click", () => {
-  console.log("hi");
+class Task {
+  constructor(title, completed) {
+    this.title = title;
+    this.completed = completed;
+  }
+
+  toggle() {
+    this.completed = !this.completed;
+  }
+
+  render() {
+    const task = document.createElement("li");
+    const checkbox = document.createElement("div");
+    const title = document.createElement("div");
+
+    if (this.completed) {
+      checkbox.className = "checkbox checked";
+    } else {
+      checkbox.className = "checkbox";
+    }
+
+    title.className = "title";
+    title.textContent = this.title;
+
+    task.appendChild(checkbox);
+    task.appendChild(title);
+
+    taskContainer.appendChild(task);
+  }
+}
+
+const taskContainer = document.querySelector(".task-container");
+
+let taskList = [
+  new Task("Walk Doggo", false),
+  new Task("Seven Best Doggo", true),
+  new Task("Groceries", false),
+];
+
+taskList.forEach((item) => {
+  item.render();
+  console.log(item);
+});
+
+const checkboxes = document.querySelectorAll(".checkbox");
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener("click", () => {});
 });
