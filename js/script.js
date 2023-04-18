aae097dc652a96bc5ab871a6b099373f36a6736d
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-04-18 11:53:56 -0500

    Create randerTasks() function
    The function takes an array of tasks and renders it on the HTML task
    container. Adding the corresponding event listeners (toggle & close)
    that apply the changes to the array, when the changes are applied, the
    function is called again to update the changes on screen

--- js/script.js
+++ js/script.js
@@ -7,42 +7,53 @@ class Task {
   toggle() {
     this.completed = !this.completed;
   }
-
-  render() {
-    const task = document.createElement("li");
-    const checkbox = document.createElement("div");
-    const title = document.createElement("div");
-
-    if (this.completed) {
-      checkbox.className = "checkbox checked";
-    } else {
-      checkbox.className = "checkbox";
-    }
-
-    title.className = "title";
-    title.textContent = this.title;
-
-    task.appendChild(checkbox);
-    task.appendChild(title);
-
-    taskContainer.appendChild(task);
-  }
 }
 
 const taskContainer = document.querySelector(".task-container");
 
-let taskList = [
+let tasks = [
   new Task("Walk Doggo", false),
-  new Task("Seven Best Doggo", true),
+  new Task("Seven Best Doggo", false),
   new Task("Groceries", false),
 ];
 
-taskList.forEach((item) => {
-  item.render();
-  console.log(item);
-});
+// takes a list of tasks and renders it on screen
+// with toggle and close functionality
+function renderTasks() {
+  taskContainer.innerHTML = "";
+  tasks.forEach((task, index) => {
+    // create list element with content & append to container
+    const taskList = document.createElement("li");
+    const taskCheckbox = document.createElement("div");
+    const taskTitle = document.createElement("div");
+    const taskClose = document.createElement("div");
+    taskCheckbox.className = "checkbox";
+    taskTitle.className = "title";
+    taskClose.className = "close";
+    taskTitle.textContent = task.title;
+    taskClose.textContent = "x";
+    taskList.appendChild(taskCheckbox);
+    taskList.appendChild(taskTitle);
+    taskList.appendChild(taskClose);
+    taskContainer.appendChild(taskList);
 
-const checkboxes = document.querySelectorAll(".checkbox");
-checkboxes.forEach((checkbox) => {
-  checkbox.addEventListener("click", () => {});
-});
+    // if task is completed add checked
+    if (task.completed) {
+      taskCheckbox.classList.add("checked");
+    }
+
+    // if checkbox is clicked, toggle item and render again
+    taskCheckbox.addEventListener("click", () => {
+      task.toggle();
+      renderTasks();
+    });
+
+    // if x is clicked, remove item and render again
+    taskClose.addEventListener("click", () => {
+      tasks.splice(index, 1);
+      renderTasks();
+    });
+  });
+}
+
+renderTasks();
