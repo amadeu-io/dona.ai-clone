5439c713a9f7749e71876588f84ed7326bd7daba
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-04-20 14:13:04 -0500

    Create renderSidebar() function
    The function takes the masterArray and renders the titles to
    the sidebar

--- js/script.js
+++ js/script.js
@@ -18,17 +18,15 @@ let masterArray = [
   },
 ];
 
-const left = document.querySelector("left");
+const left = document.querySelector(".left");
+const sidebar = document.querySelector(".sidebar");
 
 const right = document.querySelector(".right");
 const list = document.querySelector(".list");
 
 const id = 0;
 
-// renderSidebar()
-// do stuff masterArray
-
-// renders list
+// renders list with id
 function renderList(id) {
   list.innerHTML = "";
   masterArray[id].list.forEach((item, index) => {
@@ -38,3 +36,18 @@ function renderList(id) {
     list.appendChild(task);
   });
 }
+
+// renders sidebar
+function renderSidebar() {
+  sidebar.innerHTML = "";
+  masterArray.forEach((item, index) => {
+    console.log(item.title);
+    const title = document.createElement("li");
+    title.className = "title";
+    title.textContent = item.title;
+    sidebar.appendChild(title);
+  });
+}
+
+renderList(id);
+renderSidebar();
