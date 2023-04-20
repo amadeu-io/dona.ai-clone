46f3913973276daa9828964d6aa0ae8a10d51f96
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-04-20 14:03:20 -0500

    Create renderList() function
    The function takes the masterArray and randers the contents
    of the id matching list on the page

--- js/script.js
+++ js/script.js
@@ -3,17 +3,38 @@
 let masterArray = [
   {
     title: "Groceries",
-    tasks: ["Bread", "Milk", "Broccoli"],
+    list: ["Bread", "Milk", "Broccoli"],
     id: 0,
   },
   {
     title: "Doggo",
-    tasks: ["Seven Best Doggo", "Dog", "Smart Doggo"],
+    list: ["Seven Best Doggo", "Dog", "Smart Doggo"],
     id: 1,
   },
   {
     title: "Goals",
-    tasks: ["Travel The World", "Fight The Inner Weakness"],
+    list: ["Travel The World", "Fight The Inner Weakness"],
     id: 2,
   },
 ];
+
+const left = document.querySelector("left");
+
+const right = document.querySelector(".right");
+const list = document.querySelector(".list");
+
+const id = 0;
+
+// renderSidebar()
+// do stuff masterArray
+
+// renders list
+function renderList(id) {
+  list.innerHTML = "";
+  masterArray[id].list.forEach((item, index) => {
+    const task = document.createElement("li");
+    task.className = "task";
+    task.textContent = item;
+    list.appendChild(task);
+  });
+}
