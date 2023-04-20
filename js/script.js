2dd6659ee738dd842b54843c7cd2da06bb7c1d26
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-04-20 12:47:26 -0500

    Create masterArray example

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-// new starting point
+// masterArray
+
+let masterArray = [
+  {
+    title: "Groceries",
+    tasks: ["Bread", "Milk", "Broccoli"],
+    id: 0,
+  },
+  {
+    title: "Doggo",
+    tasks: ["Seven Best Doggo", "Dog", "Smart Doggo"],
+    id: 1,
+  },
+  {
+    title: "Goals",
+    tasks: ["Travel The World", "Fight The Inner Weakness"],
+    id: 2,
+  },
+];
